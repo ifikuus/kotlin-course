c9b72dde59dec15998f88c07db380231ed2accb5
ifikuus <a.s.pospelova.s.a@gmail.com>
2026-09-07 00:19:03 +0300

    3rd commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,7 +6,8 @@ fun main() {
     val name = "Kotlin"
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
-    println("Heil, " + name + "!")
+
+    println("I love " + name + "!")
 
 
 
